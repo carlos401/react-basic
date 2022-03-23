537b0d78b9a0890a993aa038035f3f5fbd43a92e
Carlos Delgado Rojas <delgadocarlos401@protonmail.com>
2022-03-22 18:47:27 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React from 'react';
+/*App.js*/
+import React, { Component } from "react";
+import "./App.css";
+//Import all needed Component for this tutorial
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+  Redirect
+} from "react-router-dom";
 
-function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+class App extends Component {
+  render() {
+    return (
+      <Router>
+       {/*All our Routes goes here!*/}
+       <Route path="/" component={} />
+      </Router>
+    );
+  }
 }
 
 export default App;
